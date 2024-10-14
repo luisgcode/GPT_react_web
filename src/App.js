f7437dbd26c4271f0c292341c3767648403424b5
luisgcode <luisgcode@gmail.com>
2024-10-14 12:23:10 -0400

    progress

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Navbar from "./components/navbar/Navbar";
 
 // ^ From containers
 import Blog from "./containers/blog/Blog";
-
+import Features from "./containers/features/Features";
 import Footer from "./containers/footer/Footer";
 import Header from "./containers/header/Header";
 import Possibility from "./containers/possibility/Possibility";
@@ -28,6 +28,7 @@ function App() {
 
       <Brand />
       <Whatgpt3 />
+      <Features />
       <Possibility />
       <Cta />
       <Blog />
